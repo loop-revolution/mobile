0ccf806db100111a0492ebba29f715869dc2cc98
Louis Birla <louis.birla@gmail.com>
2021-01-31 15:33:43 -0600

    build(api): use main API endpoint

--- src/api/client.ts
+++ src/api/client.ts
@@ -1,12 +1,12 @@
-import AsyncStorage from "@react-native-async-storage/async-storage"
-import { createClient } from "urql"
+import AsyncStorage from "@react-native-async-storage/async-storage";
+import { createClient } from "urql";
 
 export const client = createClient({
-    url: 'https://api-pr-100-zpsq.onrender.com',
-    fetchOptions: async () => {
-        const token = await AsyncStorage.getItem('token')
-        return {
-            headers: { authorization: token ? `${token}` : '' },
-        }
-    },
-})
+  url: "https://api.loop.page",
+  fetchOptions: async () => {
+    const token = await AsyncStorage.getItem("token");
+    return {
+      headers: { authorization: token ? `${token}` : "" },
+    };
+  },
+});
