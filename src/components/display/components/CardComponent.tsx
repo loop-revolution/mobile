39e0c8140d22300daf7a222c25311bc543dc9831
Salman Maredia <salmanaly1492@gmail.com>
2021-03-08 20:37:10 -0500

    patch react native paper list accordion

--- src/components/display/components/CardComponent.tsx
+++ src/components/display/components/CardComponent.tsx
@@ -1,9 +1,9 @@
 import React, { useRef, useState } from 'react'
 import { CardArgs } from 'display-api'
-import { Card, List } from 'react-native-paper'
+import { Card, IconButton, List } from 'react-native-paper'
 import colors from '../../../utils/colors'
 import { ComponentDelegate } from '../ComponentDelegate'
-import { StyleSheet } from 'react-native'
+import { StyleSheet, View } from 'react-native'
 import { getComponentIcon } from '../../../utils/utils'
 import { MaterialCommunityIcons } from '@expo/vector-icons'
 import routes from '../../../navigation/routes'
@@ -18,12 +18,12 @@ export const CardComponent = ({ header, color, content }: CardArgs) => {
 	const navigation = useNavigation()
 	color = color || colors.primary
 	const LeftContent = () => <MaterialCommunityIcons color={color} name={getComponentIcon(header)} size={25} />
-	// const RightContent = (content: any) => (
-	// 	<View style={styles().titleOptions}>
-	// 		<IconButton color={color} icon='dots-horizontal' onPress={() => { navigation.navigate(routes.BLOCK_PAGE, {blockId: header.block_id}) }} />
-	// 		<IconButton color={color} icon='chevron-down' />
-	// 	</View>
-	// )
+	const RightContent = (props: any) => (
+		<View style={styles().titleOptions}>
+			<IconButton color={color} icon='dots-horizontal' onPress={() => { navigation.navigate(routes.BLOCK_PAGE, { blockId: header.block_id }) }} />
+			<IconButton color={color} icon='chevron-down' />
+		</View>
+	)
 
 	return (
 		<Card style={styles(color).cardContainer}>
@@ -33,6 +33,7 @@ export const CardComponent = ({ header, color, content }: CardArgs) => {
 					titleStyle={styles().title}
 					title={header.title}
 					left={LeftContent}
+					right={RightContent}
 					expanded={isExpanded}
 					onPress={() => {
 						//TODO: Temporary solution, there is a PR active to configure the
@@ -48,10 +49,10 @@ export const CardComponent = ({ header, color, content }: CardArgs) => {
 					</Card.Content>
 				</List.Accordion>
 			) : (
-				<Card.Content style={styles().cardContent}>
-					<ComponentDelegate component={content} />
-				</Card.Content>
-			)}
+					<Card.Content style={styles().cardContent}>
+						<ComponentDelegate component={content} />
+					</Card.Content>
+				)}
 			{header.menu && <BottomMenu ref={menuRef} menu={header.menu} />}
 		</Card>
 	)
