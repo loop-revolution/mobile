4c21a57423f96707ae3d0fd862761174fbf250e3
Salman Maredia <salmanaly1492@gmail.com>
2021-03-28 18:46:28 -0400

    Added display component to html conversion for rich text component

--- src/components/display/components/RichTextComponent.tsx
+++ src/components/display/components/RichTextComponent.tsx
@@ -7,32 +7,18 @@ import { setMethodVariable } from "../method"
 import { View, StyleSheet, Keyboard } from 'react-native'
 import { globalStyles } from '../../../utils/styles'
 import { ScrollView, TouchableWithoutFeedback } from 'react-native-gesture-handler'
-import { RichEditor, RichToolbar } from 'react-native-pell-rich-editor'
+import { actions, RichEditor, RichToolbar } from 'react-native-pell-rich-editor'
 import { KeyboardAwareView } from 'react-native-keyboard-aware-view'
 import colors from '../../../utils/colors'
 import { SafeAreaView } from 'react-native-safe-area-context'
-import { Portal } from 'react-native-paper'
+import { IconButton, Portal } from 'react-native-paper'
 import { jsonToHtmlConversion } from '../../../utils/htmlJsonConversion'
+import { MaterialCommunityIcons } from '@expo/vector-icons'
 
 export const RichTextComponent = ({ content, editable = false, name, save, on_enter }: RichTextArgs) => {
 
 	let richText = useRef();
-	const [value, doSetValue] = useState<string[]>(content.map(jsonToHtmlConversion))
-
-
-	const initHTML = `<div>
-	This is a rich text editor 
-	<b><i>This is a rich text editor</i></b> 
-	This is a rich text editor 
-	<u>This is a rich text editor</u><b><i><u> 
-	This is a rich text editor 
-	</u></i></b></div><div>
-	This is a rich text editor 
-	<b><i>This is a rich text editor</i></b> 
-	This is a rich text editor 
-	<u>This is a rich text editor</u><b><i><u> 
-	This is a rich text editor 
-	</u></i></b></div>`;
+	const [value, setValue] = useState<string>(jsonToHtmlConversion(content))
 	const [isFocused, setFocused] = useState<boolean>(false)
 
 	return (
@@ -49,7 +35,7 @@ export const RichTextComponent = ({ content, editable = false, name, save, on_en
 				onChange={(text) => {
 					console.log("text: ", text)
 				}}
-				initialContentHTML={initHTML}
+				initialContentHTML={value}
 				editorStyle={{ backgroundColor: 'transparent' }}
 				style={[{ backgroundColor: 'transparent' }]}
 				placeholder='Start typing...'
@@ -63,6 +49,16 @@ export const RichTextComponent = ({ content, editable = false, name, save, on_en
 						selectedIconTint={colors.accent}
 						disabledIconTint={'#bfbfbf'}
 						editor={richText}
+						actions={[
+							actions.keyboard,
+							actions.setBold,
+							actions.setItalic,
+							actions.setUnderline,
+							actions.setStrikethrough,
+							actions.removeFormat,
+							actions.undo,
+							actions.redo,
+						]}
 					/>
 				</View>
 			}
--- src/utils/htmlJsonConversion.ts
+++ src/utils/htmlJsonConversion.ts
@@ -1,8 +1,10 @@
 import React, { useRef } from 'react'
 import { TextArgs, TextComponent } from "display-api"
 const json2html = require('node-json2html');
+import escapeHtml from 'escape-html'
+import { Node, Text } from 'slate'
 
-export const jsonToHtmlConversion = (component: TextComponent): string => {
+export const jsonToHtmlConversion = (components: TextComponent[]): string => {
     // let textArgs = component.args
     // const { text, bold, underline, monospace, preset, strikethrough, italic } = textArgs
     // let heading = false
@@ -10,27 +12,97 @@ export const jsonToHtmlConversion = (component: TextComponent): string => {
     //     heading = true
     // }
 
-    let template = {
-        '<>': 'div', 'html': addTag({ ...component.args })
+    // let template = {
+    //     '<>': 'div', 'html': addTag({ ...component.args })
+    // }
+
+    // console.log("json2html: ", json2html)
+    // let html = json2html.transform({}, template)
+    // return html
+
+    // const convertComponentToSlateText = (component: TextComponent) => {
+    //     let textArgs = component.args
+    //     const { text, bold, underline, monospace, preset, strikethrough, italic } = textArgs
+    //     let heading = false
+    //     if (preset === "Heading") {
+    //         heading = true
+    //     }
+    //     return {
+    //         bold,
+    //         code: monospace,
+    //         heading,
+    //         italic,
+    //         strikethrough,
+    //         text,
+    //         underline,
+    //     }
+    // }
+
+    // let slateTexts: Text[] = components.map(convertComponentToSlateText)
+    // let slateObject = { type: 'paragraph', children: slateTexts }
+    // let html = serialize(slateObject)
+
+    if (components.length == 0) {
+        return ''
     }
 
-    console.log("json2html: ", json2html)
-    let html = json2html.transform({}, template)
-    return html
+    let html = ''
+    components.map(component => {
+        html = `${html}${buildHtmlTag(component.args)}`
+    })
+    return `<div>${html}</div>`
 }
 
-const addTag = (args: TextArgs) => {
+const buildHtmlTag = (args: TextArgs) => {
+    if (args.preset === 'Heading') {
+        args.preset = null
+        return `<H2>${buildHtmlTag(args)}</H2>`
+    }
     if (args.bold) {
         args.bold = false
-        return [{ '<>': 'b', html: addTag(args) }]
+        return `<b>${buildHtmlTag(args)}</b>`
     }
     if (args.italic) {
         args.italic = false
-        return { '<>': 'b', html: [addTag(args)] }
+        return `<em>${buildHtmlTag(args)}</em>`
     }
     if (args.underline) {
         args.underline = false
-        return { '<>': 'b', html: [addTag(args)] }
+        return `<u>${buildHtmlTag(args)}</u>`
+    }
+    if (args.strikethrough) {
+        args.strikethrough = false
+        return `<del>${buildHtmlTag(args)}</del>`
+    }
+    if (args.monospace) {
+        args.monospace = false
+        return `<code style="background-color:lightgray";>${buildHtmlTag(args)}</code>`
     }
     return args.text
 }
+
+// const serialize = node => {
+//     debugger
+//     if (Text.isText(node)) {
+//         return escapeHtml(node.text)
+//     }
+
+//     const children = node.children.map(n => serialize(n)).join('')
+
+//     switch (node) {
+//         case 'quote':
+//             return `< blockquote > <p>${children} </p></blockquote > `
+//         case 'paragraph':
+//             return `< p > ${children} </p>`
+//         case 'link':
+//             return `<a href="${escapeHtml(node.url)}">${children}</a>`
+//         case 'bold':
+//             return `<b><p>${children}</p></b>`
+//         case 'italic':
+//             return `<em>${children}</em>`
+//         case 'underline':
+//             return `<u>${children}</u>`
+//         default:
+//             return children
+//     }
+// }
